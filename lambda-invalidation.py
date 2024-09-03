a57e59578e4e15ffae99cc4e96e580cc23a5e9be
Jona Baez <94jjba@gmail.com>
2024-09-02 21:44:52 -0600

    edicion de instrucciones

--- lambda-invalidation.py
+++ lambda-invalidation.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
         path = "/" + items["s3"]["object"]["key"]
     print(path)
     client = boto3.client('cloudfront')
-    invalidation = client.create_invalidation(DistributionId='tudistribucion',
+    invalidation = client.create_invalidation(DistributionId='EG0ZD1O7ZS3PU',
         InvalidationBatch={
             'Paths': {
                 'Quantity': 1,
